e4ff76ffb3eae493a1e23dff0064625d8aa89a38
Renato <renato.henrique_98@hotmail.com>
2023-04-22 20:44:28 -0300

    changes

---
+++ src/main/java/com/gihub/throyer/Main.java
@@ -0,0 +1,12 @@
+package com.gihub.throyer;
+
+import static com.gihub.throyer.Game.start;
+
+/**
+ * Hello world!
+ */
+public class Main {
+    public static void main(String[] args) {
+        start(args);
+    }
+}
